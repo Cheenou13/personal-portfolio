cf44b150735ef78ceeef7e2c057c6b6e32fdd19d
Cheenou Xiong <cheenou.xiong@fewidev.com>
2023-07-15 19:21:40 -0500

    add tailwind css built in styling classes

---
+++ client/src/router/index.js
@@ -0,0 +1,23 @@
+// import {createRouter, createWebHistory} from 'vue-router'
+// import ChatPage from '../views/ChatPage.vue'
+// import DashBoardPage from '../views/DashBoardPage.vue'
+
+// const routes = [
+//     {
+//         path: '/',
+//         name: 'Home',
+//         component: ChatPage
+//     },
+//     {
+//         path: '/dashboard',
+//         name: 'DashBoardPage',
+//         component: DashBoardPage
+//     }
+// ]
+
+// const router = createRouter({
+//     history: createWebHistory(import.meta.env.BASE_URL),
+//     routes
+// })
+
+// export default router
