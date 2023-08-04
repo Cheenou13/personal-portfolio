e89300d0a2570c3992662c442d2cc46f7f1c3a5a
chee <xiong726@uwm.edu>
2023-08-04 17:43:15 -0500

    created home page component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,22 @@
-// import {createRouter, createWebHistory} from 'vue-router'
-// import ChatPage from '../views/ChatPage.vue'
-// import DashBoardPage from '../views/DashBoardPage.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from '../views/Home.vue'
 
-// const routes = [
-//     {
-//         path: '/',
-//         name: 'Home',
-//         component: ChatPage
-//     },
-//     {
-//         path: '/dashboard',
-//         name: 'DashBoardPage',
-//         component: DashBoardPage
-//     }
-// ]
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    // {
+    //     path: '/dashboard',
+    //     name: 'DashBoardPage',
+    //     component: DashBoardPage
+    // }
+]
 
-// const router = createRouter({
-//     history: createWebHistory(import.meta.env.BASE_URL),
-//     routes
-// })
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+})
 
-// export default router
+export default router
